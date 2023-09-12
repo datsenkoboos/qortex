781f2b13e35cecdffb87f739d922d7db8df6b6c9
datsenkoboos <adacenkoboos@gmail.com>
2023-09-12 20:36:00 +0700

    starting the work

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { Quasar } from 'quasar'
+
+// Import icon libraries
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+})
+
+app.mount('#app')
