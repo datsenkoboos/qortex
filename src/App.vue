49cd98f3103014e6688f25c54fe62d0ea4e70227
datsenkoboos <adacenkoboos@gmail.com>
2023-09-13 14:11:09 +0700

    base files done

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router';
+import { AppHeader } from '@/components/modules';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <AppHeader />
+  <main class="grow px-[10%] py-10">
+    <RouterView />
+  </main>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,26 @@
-import { Quasar } from 'quasar'
+import { Quasar } from 'quasar';
 
 // Import icon libraries
-import '@quasar/extras/roboto-font/roboto-font.css'
-import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/roboto-font/roboto-font.css';
+import '@quasar/extras/material-icons/material-icons.css';
 
 // Import Quasar css
-import 'quasar/src/css/index.sass'
+import 'quasar/src/css/index.sass';
 
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 app.use(Quasar, {
-    plugins: {}, // import Quasar plugins and add here
-})
+  plugins: {}, // import Quasar plugins and add here
+});
 
-app.mount('#app')
+app.mount('#app');
