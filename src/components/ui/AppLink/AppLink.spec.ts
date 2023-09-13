d43ca135622b0ae0a0d7d2f9b563760591904c4f
datsenkoboos <adacenkoboos@gmail.com>
2023-09-13 10:12:42 +0700

    AppLink done

---
+++ src/components/ui/AppLink/AppLink.spec.ts
@@ -0,0 +1,17 @@
+import { shallowMount } from '@vue/test-utils';
+import AppLink from './AppLink.vue';
+import { describe, it, expect } from 'vitest';
+
+describe('AppLink', () => {
+    describe('props', () => {
+        it('to - should render RouterLink with set "to" attr', () => {
+            const testTo = 'test-to';
+            const wrapper = shallowMount(AppLink, {
+                props: {
+                    to: testTo
+                }
+            })
+            expect(wrapper.get('[data-testid=routerLink]').attributes('to')).toBe(testTo)
+        })
+    })
+})
---
+++ src/components/ui/AppLink/AppLink.vue
@@ -0,0 +1,12 @@
+<template>
+    <RouterLink :to="to" class="font-semibold hover:text-blue-700 transition-all" data-testid="routerLink">
+        <slot />
+    </RouterLink>
+</template>
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+
+const props = defineProps<{
+    to: string;
+}>()
+</script>
