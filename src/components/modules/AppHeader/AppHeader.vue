ad72ec647346d2c7ae3643c7a8b8ff795ce10bcd
datsenkoboos <adacenkoboos@gmail.com>
2023-09-13 10:14:00 +0700

    AppHeader done

---
+++ src/components/modules/AppHeader/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="border-b-[1px] h-[64px] flex items-center justify-between px-[10%]">
+    <AppLogo />
+    <nav class="font-semibold flex gap-5">
+      <AppLink to="/">Исполнители</AppLink>
+      <AppLink to="/albums">Альбомы</AppLink>
+      <AppLink to="/add">Добавить песню</AppLink>
+    </nav>
+  </header>
+</template>
+<script setup lang="ts">
+import { AppLink, AppLogo } from '@/components/ui'
+</script>
